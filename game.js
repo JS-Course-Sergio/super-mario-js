bcaed514ccc3865b076c537a27a8c5e73bcf8a5d
Sergio Barreras <sergiobg.isc@gmail.com>
2024-07-17 18:45:48 -0700

    game config

---
+++ game.js
@@ -0,0 +1,18 @@
+const config = {
+  type: Phaser.AUTO,
+  with: 256,
+  height: 244,
+  backgroundColor: "white",
+  parent: "game",
+  scene: {
+    preload,
+    create,
+    update,
+  },
+};
+
+function preload() {}
+
+function create() {}
+
+function update() {}
